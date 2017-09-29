bff755c68b4afe9c8f8bdbe8f0f597ae9a6fe019
nui <nui@xxx.com>
2017-09-29 15:31:09 +0700

    Add download cache & froce

--- deploy.sh
+++ deploy.sh
@@ -2,37 +2,74 @@
 file_name=""
 COMPUTER_NAME=$(scutil --get ComputerName);
 if [ $1 == "-d" ]; then
-	
-	if [ -f $2 ]
-	then
-		rm $2
-	else
-		echo "$file not found."
-	fi
+
 	until $(curl -uadmin:password -X GET "http://10.215.99.238:8081/artifactory/api/storage/mobile-plus/iOS/Development" | python -m json.tool > archives.json); do
 		sleep 1
 	done
 
 	THE_FILE_NAME=$2;
 	if [ "$2" == "last" ]; then
-		THE_FILE_NAME=$(cat archives.json | ./jq '[.children[].uri]' | ./jq 'reverse' | ./jq 'limit(1;.[])' | sed -l 's/"\///g' | sed -l 's/"//g');
+			THE_FILE_NAME=$(cat archives.json | ./jq '[.children[].uri]' | ./jq 'reverse' | ./jq 'limit(1;.[])' | sed -l 's/"\///g' | sed -l 's/"//g');
+			echo "=================================================================="
+			echo "DOWNFILE FILE ========>".$THE_FILE_NAME;
+			echo "=================================================================="
+	else
+			sleep 1
+	fi
+
+	echo "FILE NAME = ".$THE_FILE_NAME
+	if [ -f $THE_FILE_NAME ]
+	then
+		echo "======> ON CACHE <======"
+	else
+		
+
+		
+		echo "Downloading.....";
+		until $(wget "http://10.215.99.238:8081/artifactory/mobile-plus/iOS/Development/$THE_FILE_NAME"); do
+			echo "Download Success ^_^";
+	    	sleep 1
+		done
+	fi
+	for DEVICE in $(instruments -s devices | grep -v Simulator | grep -v Known | grep -v "$COMPUTER_NAME" |cut -f 2 -d [ | cut -f 1 -d ]
+			);
+	do
+	   	./fruitstrap -i $DEVICE -b $THE_FILE_NAME
+	done
+elif [ $1 == "-df" ]; then
+
+		until $(curl -uadmin:password -X GET "http://10.215.99.238:8081/artifactory/api/storage/mobile-plus/iOS/Development" | python -m json.tool > archives.json); do
+		sleep 1
+		done
+
+		THE_FILE_NAME=$2;
+		if [ "$2" == "last" ]; then
+			THE_FILE_NAME=$(cat archives.json | ./jq '[.children[].uri]' | ./jq 'reverse' | ./jq 'limit(1;.[])' | sed -l 's/"\///g' | sed -l 's/"//g');
+		else
+			sleep 1
+		fi
+
+		if [ -f $THE_FILE_NAME ]
+		then
+			rm $THE_FILE_NAME
+		else
+			sleep 1
+		fi
+		
 		echo "=================================================================="
 		echo "DOWNFILE FILE ========>".$THE_FILE_NAME;
 		echo "=================================================================="
-	else
-		sleep 1
-	fi
-	echo "Downloading.....";
-	until $(wget "http://10.215.99.238:8081/artifactory/mobile-plus/iOS/Development/$THE_FILE_NAME"); do
-		echo "Download Success ^_^";
-    	# $file_name=$THE_FILE_NAME
-	done
+		echo "Downloading.....";
+		until $(wget "http://10.215.99.238:8081/artifactory/mobile-plus/iOS/Development/$THE_FILE_NAME"); do
+			echo "Download Success ^_^";
+	    	sleep 1
+		done
 
-	for DEVICE in $(instruments -s devices | grep -v Simulator | grep -v Known | grep -v "$COMPUTER_NAME" |cut -f 2 -d [ | cut -f 1 -d ]
-		);
-	do
-   		./fruitstrap -i $DEVICE -b $THE_FILE_NAME
-	done
+		for DEVICE in $(instruments -s devices | grep -v Simulator | grep -v Known | grep -v "$COMPUTER_NAME" |cut -f 2 -d [ | cut -f 1 -d ]
+			);
+		do
+	   		./fruitstrap -i $DEVICE -b $THE_FILE_NAME
+		done
 elif [ $1 == "-l" ]; then
 	until $(curl -uadmin:password -X GET "http://10.215.99.238:8081/artifactory/api/storage/mobile-plus/iOS/Development" | python -m json.tool > archives.json); do
 		sleep 1
